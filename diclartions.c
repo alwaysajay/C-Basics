c722c7db4ac64a4dfbdaf4a9cfdc740df3339481
alwaysajay <92288156+alwaysajay@users.noreply.github.com>
2021-10-20 10:38:44 +0530

    Add files via upload

---
+++ diclartions.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	// variable declaration
+	int age;
+	float cgpa;
+	char intial;
+	
+	// intialazation
+	age    = 20;
+	cgpa   = 8.6;
+	intial = 'A';
+	
+	// out put
+	printf("age of the student =%d\n",age);
+	printf("cgpa of the student=%f\n",cgpa);
+	printf("intial of the student=%c",intial);
+	
+}
