fb9cbf79991c88d9b6d88d34809e7079a2da3883
alwaysajay <92288156+alwaysajay@users.noreply.github.com>
2021-11-03 10:53:37 +0530

    Add files via upload

---
+++ celsius.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	int cel,far;
+	cel=10;
+	far=(cel*9/5)+32;
+	printf("%.d of celsuis is %.d  of fahrenheit",cel,far);
+}
---
+++ circle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+   float area,r;
+	r=5;
+	area=(3.14)*r*r;
+	printf(" area of circle %f when  radius is %.2f",area,r);
+	
+}
